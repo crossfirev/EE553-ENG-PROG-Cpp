da9a4c8e7ee162877485ff7cdfdc330b604242a6
crossfirev <lepismatthew@gmail.com>
2025-09-05 13:05:50 -0400

    pre-commit changes

--- homework/hw01/main.cpp
+++ homework/hw01/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-#include <cmath>
 
-using namespace std;
+using namespace std; // if not used, must write std::cout or std::endl
 
 // --- Problem 1 ---
 int sumUpTo(int sumTo)
 {
-    int sum = 0; 
-    for(int i = 1; i <= sumTo; i++)
+    int sum = 0;
+    for (int i = 1; i <= sumTo; i++)
     {
         sum += i;
     }
@@ -17,20 +16,20 @@ int sumUpTo(int sumTo)
 // --- Problem 2 ---
 float sumReciprocalNormal(int sumTo_denominator)
 {
-    float sum = 0; 
-    for(int i = 1; i <= sumTo_denominator; i++)
+    float sum = 0;
+    for (int i = 1; i <= sumTo_denominator; i++)
     {
-        sum += 1.0/i;
+        sum += 1.0 / i;
     }
     return sum;
 }
 
 float sumReciprocalRev(int sumFrom_denominator)
 {
-    float sum = 0; 
-    for(int i = sumFrom_denominator; i >= 1; i--)
+    float sum = 0;
+    for (int i = sumFrom_denominator; i >= 1; i--)
     {
-        sum += 1.0/i;
+        sum += 1.0 / i;
     }
     return sum;
 }
@@ -38,25 +37,26 @@ float sumReciprocalRev(int sumFrom_denominator)
 // --- Problem 3 ---
 float sumMysteryNormal(int sumTo_denominator)
 {
-    float sum = 0; 
-    for(int i = 1; i <= sumTo_denominator; i++)
+    float sum = 0;
+    for (int i = 1; i <= sumTo_denominator; i++)
     {
-        sum += 1.0/(i*i);
+        sum += 1.0 / (i * i);
     }
     return sum;
 }
 
 float sumMysteryRev(int sumFrom_denominator)
 {
-    float sum = 0; 
-    for(int i = sumFrom_denominator; i >= 1; i--)
+    float sum = 0;
+    for (int i = sumFrom_denominator; i >= 1; i--)
     {
-        sum += 1.0/(i*i);
+        sum += 1.0 / (i * i);
     }
     return sum;
 }
 
-int main() {
+int main()
+{
     // Problem One
     cout << sumUpTo(100) << '\n';
 
@@ -64,13 +64,13 @@ int main() {
     cout << sumReciprocalNormal(100) << '\n';
     cout << sumReciprocalRev(100) << '\n';
 
-    float sum1 = sumReciprocalNormal(100);  // compute the sum 1/1 + 1/2 + 1/3 + 1/4 + 1/5 + ... + 1/100 =5.18....
-    float sum2 = sumReciprocalRev(100);     // compute the sum 1/100 + 1/99 + 1/98 + ... + 1/1 = 5.18
+    float sum1 = sumReciprocalNormal(100); // compute the sum 1/1 + 1/2 + 1/3 + 1/4 + 1/5 + ... + 1/100 =5.18....
+    float sum2 = sumReciprocalRev(100); // compute the sum 1/100 + 1/99 + 1/98 + ... + 1/1 = 5.18
     cout << sum1 << '\n';
     cout << sum2 << '\n';
     cout << (sum1 - sum2) << '\n';
 
-    // sum1 - sum2 isn't zero. This is expected, a result of floating point rounding. This error can be ~reduced~ with `double` or `long double` implementations.
+    // sum1 - sum2 isn't zero. This is expected, a result of floating point rounding. This error can be ~reduced with `double` or `long double` implementations.
 
     // Problem Three
     cout << sumMysteryNormal(100) << '\n';
