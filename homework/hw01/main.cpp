454cda4bb66d361f2d4ed6fa532b8accfd342635
crossfirev <lepismatthew@gmail.com>
2025-09-05 12:22:09 -0400

    Problem 1 complete

--- homework/hw01/main.cpp
+++ homework/hw01/main.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <cmath>
-//#include "functionSum.h"
-
 
 using namespace std; // if not used, must write std::cout or std::endl
 
-
+// --- Problem 1 ---
+int sumUpTo(int sumTo)
+{
+    int sum = 0; 
+    for(int i = 1; i <= sumTo; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
 
 int main() {
     // Problem One
     cout << sumUpTo(100) << '\n';
 
     // Problem Two
-    cout << sumReciprocalNormal(100) << '\n';
-    cout << sumReciprocalRev(100) << '\n';
+    // cout << sumReciprocalNormal(100) << '\n';
+    // cout << sumReciprocalRev(100) << '\n';
 
     //float sum1;// compute the sum 1/1 + 1/2 + 1/3 + 1/4 + 1/5 + ... + 1/100 =5.18....
     //float sum2;// compute the sum 1/100 + 1/99 + 1/98 + ... + 1/1 = 5.18
@@ -26,8 +33,8 @@ int main() {
     // inclusive: 1*1 + 2*2 + 3*3 + 4*4 + 5*5 +6*6 + 7*7
 
     // Problem Three
-    cout << sumMysteryNormal(100) << '\n';
-    cout << sumMysteryRev(100) << '\n';
+    // cout << sumMysteryNormal(100) << '\n';
+    // cout << sumMysteryRev(100) << '\n';
     // mystery1sum
     // be careful! if you calculate as int this will not work. use float!
     // compute 1.0/(1*1) + 1.0/(2.0*2.0) + 1/(3*3) + ... + 1/(n*n)  n-> infinity
