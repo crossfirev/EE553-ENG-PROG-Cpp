466f80f625b36bb4a96dc979369cf856ce861ebc
crossfirev <lepismatthew@gmail.com>
2025-09-19 10:56:15 -0400

    fixing comment

--- homework/hw02/src/functionPrime.cpp
+++ homework/hw02/src/functionPrime.cpp
@@ -2,7 +2,7 @@ bool isPrime(const int primeCandidate)
 {
     for (int i = primeCandidate-1; i > 1; i--)
     {
-        // if dividing by i  gives no remainder, it isn't prime.
+        // if dividing by i gives no remainder, it isn't prime.
         if (primeCandidate % i == 0)
         {
             return false;
