0b8af4ac2ada81773a628c597ff909452d6b2a17
crossfirev <lepismatthew@gmail.com>
2025-11-05 23:42:53 -0500

    Zebra and ZebraKid Class implemented

--- homework/hw06/src/main_hw06.cpp
+++ homework/hw06/src/main_hw06.cpp
@@ -1,88 +1,106 @@
+#include <limits>
 #include <iostream>
 #include <string>
+#include <stdexcept>
 
- 
+class Zebra
+{
+private:
+    std::string name;
 
-using namespace std;
+protected:
+    static int count;
 
-// -------------- Design your Zebra class here ---------------
-// -----------------------------------------------------------
-// name the Class Zebra()
-// name the function that count function getCount()
-/// name the function that print name getName()
-// private: create variable name as string and count as int make it static and set it externally before main()
-/// Create constructor that take directly the name
-// Once object is created inside constructor print the following
-// Hello I'm just born! my name is:  "object name" and new line
+public:
+    Zebra()
+    {
+        name = "Zebra_" + std::to_string(count);
+        count++;
 
-// Create function that called getName that return name without printing
+        std::cout << "Hello I was just born! My name is: " << getName() << ".\n";
+    }
+    Zebra(std::string in_name)
+    {
+        name = in_name;
+        count++;
+        
+        std::cout << "Hello I was just born! My name is: " << getName() << ".\n";
+    }
+    ~Zebra()
+    {
+        count--;
 
-// Make a destructor, it should reduce count and print the following
-//     Ugh! I'm dying and the current count
+        std::cout << "Ugh! I'm " << getName() << ", I'm dying. Current Count: " << getCount() << ".\n";
+    }
 
-// Create a function that called getCount that return the current count
+    static int getCount() { return count; }
+    std::string getName() { return name; }
+    
+    void setName(std::string in_name)
+    {
+        name = in_name;
+    }
+};
 
-//}; end of class
+class ZebraKid : public Zebra
+{
+public:
+    ZebraKid() : Zebra() {}
+    ZebraKid(std::string in_name) : Zebra(std::move(in_name)) {}
+};
 
-// -------------- Design your ZebraKid class here ---------------
-// -----------------------------------------------------------
-// Create a another class ZebraKid that inherit Zebra functions (getName)
-// ZebraKid only have constructor
-// initial constructor based on Zebra constructor
-// don't write any other function, you should be able to inherit Zebra function
-//}; end of class
+int Zebra::count = 0;
 
-// Set Zebra count equal zero
+void f()
+{
+    std::cout << "Current count: " << Zebra::getCount() << "\n";
+    
+    Zebra z1;
+    Zebra z2;
+    Zebra z3("Joker");
+    Zebra z4("Alice");
+    
+    std::cout << "New count: " << Zebra::getCount() << "\n";
+}
 
-// -------------- Design void function f() ---------------
-// -----------------------------------------------------------
-// Make function f() that do the following when used:
-//   - Print out the current count
-//   - add two Zebra object when called
-//    - Zebra with name Joker
-//      Zebra with name Alice
-//   - Print the new count
-// when the function is called it will construct object and when it scopes finish
-//... it will destruct and print messages of object is dying in same time
 
-// example of the main function
-int main() {
-    cout << "########" << endl;
-    cout << "Main Problem" << endl;
-    cout << "########" << endl;
+int main()
+{
+    std::cout << "########" << std::endl;
+    std::cout << "Main Problem" << std::endl;
+    std::cout << "########" << std::endl;
     // You should be able to run main() with your classes
     // No need to edit this part unless for debugging
     // print the initial count number
-    cout << Zebra::getCount() << '\n'; // zero!
+    std::cout << Zebra::getCount() << '\n'; // zero!
     // Creat object a with name Morgana
     Zebra a("Morgana");
     // Print the current count number of Zerba object
-    cout << Zebra::getCount() << '\n';
+    std::cout << Zebra::getCount() << '\n';
     // Use the function f() above
     f();
-    cout << "####" << endl;
+    std::cout << "####" << std::endl;
     // Use the function f() above again
     f();
-    cout << "####" << endl;
+    std::cout << "####" << std::endl;
     // print out the current number of zebra
-    cout << Zebra::getCount() << '\n';
+    std::cout << Zebra::getCount() << '\n';
     // Get the name of the zebra object a
-    cout << a.Zebra::getName() << '\n';
-    cout << "####" << endl;
+    std::cout << a.Zebra::getName() << '\n';
+    std::cout << "####" << std::endl;
 
     // create  ZebraKid object
     ZebraKid e("James");
     // Use the getName() form Zebra class
-    cout << e.getName() << '\n';
-    cout << Zebra::getCount() << '\n';
+    std::cout << e.getName() << '\n';
+    std::cout << Zebra::getCount() << '\n';
 
 
-    cout << "====[ end ]====" << endl;
-    cout << "               " << endl;
+    std::cout << "====[ end ]====" << std::endl;
+    std::cout << "               " << std::endl;
 
-    cout << " #### This is after the program is completely done #####" << endl;
-    cout << " ### The last remaining object should get destructed ###" << endl;
+    std::cout << " #### This is after the program is completely done #####" << std::endl;
+    std::cout << " ### The last remaining object should get destructed ###" << std::endl;
 
     return 0;
-
 }
