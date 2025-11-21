3cc116b4b8fe346b36ee3b82f5c4ec013f4241c1
crossfirev <lepismatthew@gmail.com>
2025-11-21 16:47:58 -0500

    clang-format

--- homework/hw07/src/main_hw07.cpp
+++ homework/hw07/src/main_hw07.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
 #include <fstream>
-#include <vector>
+#include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -19,11 +19,14 @@ private:
     Position position; // Units: pixels
 
 public:
-    Shape(double in_pos_x, double in_pos_y): position{in_pos_x, in_pos_y} {} // ctor using initializer list
+    Shape(double in_pos_x, double in_pos_y)
+        : position { in_pos_x, in_pos_y }
+    {
+    } // ctor using initializer list
     virtual ~Shape() = default;
 
-    virtual double area() const = 0;        // Pure virtual since this is set equal to zero.
-    virtual void draw(ostream& file) const = 0;   // Pure virtual since this is set equal to zero.
+    virtual double area() const = 0; // Pure virtual since this is set equal to zero.
+    virtual void draw(ostream& file) const = 0; // Pure virtual since this is set equal to zero.
 
     Position getPosition() const
     {
@@ -37,8 +40,13 @@ class Circle : public Shape
 private:
     double radius; // Units: pixels
     string shapeType = "Circle";
+
 public:
-    Circle(double in_pos_x, double in_pos_y, double in_radius): Shape(in_pos_x, in_pos_y), radius(in_radius) {}
+    Circle(double in_pos_x, double in_pos_y, double in_radius)
+        : Shape(in_pos_x, in_pos_y)
+        , radius(in_radius)
+    {
+    }
     ~Circle() = default;
 
     string getShapeType() const override
@@ -51,22 +59,28 @@ public:
         return PI * radius * radius;
     }
 
-    void draw(ostream &file) const override
+    void draw(ostream& file) const override
     {
         Position position = getPosition();
-        
+
         file << position.x << " " << position.y << " " << radius << " 0 360 arc\n";
         file << "stroke\n";
     }
 };
 
-class Rect: public Shape
+class Rect : public Shape
 {
 private:
     double width, height;
     string shapeType = "Rectangle";
+
 public:
-    Rect(double in_pos_x, double in_pos_y, double in_width, double in_height): Shape(in_pos_x, in_pos_y), width(in_width), height(in_height) {}
+    Rect(double in_pos_x, double in_pos_y, double in_width, double in_height)
+        : Shape(in_pos_x, in_pos_y)
+        , width(in_width)
+        , height(in_height)
+    {
+    }
     ~Rect() = default;
 
     string getShapeType() const override
@@ -74,27 +88,36 @@ public:
         return "Rectangle";
     }
 
-    double area() const override { return width * height; }
-    void draw(ostream &file) const override
+    double area() const override
+    {
+        return width * height;
+    }
+    void draw(ostream& file) const override
     {
         Position position = getPosition();
-        
+
         file << position.x << " " << position.y << " moveto\n";
-        file << position.x + width << " " << position.y << " lineto\n"; 
-        file << position.x + width << " " << position.y + height << " lineto\n"; 
-        file << position.x << " " << position.y + height << " lineto\n"; 
-        file << "closepath\n"; 
+        file << position.x + width << " " << position.y << " lineto\n";
+        file << position.x + width << " " << position.y + height << " lineto\n";
+        file << position.x << " " << position.y + height << " lineto\n";
+        file << "closepath\n";
         file << "stroke\n";
     }
 };
 
-class FilledRect: public Shape
+class FilledRect : public Shape
 {
 private:
     double width, height;
     string shapeType = "Filled Rectangle";
+
 public:
-    FilledRect(double in_pos_x, double in_pos_y, double in_width, double in_height): Shape(in_pos_x, in_pos_y), width(in_width), height(in_height){}
+    FilledRect(double in_pos_x, double in_pos_y, double in_width, double in_height)
+        : Shape(in_pos_x, in_pos_y)
+        , width(in_width)
+        , height(in_height)
+    {
+    }
     ~FilledRect() = default;
 
     string getShapeType() const override
@@ -102,16 +125,19 @@ public:
         return "Filled Rectangle";
     }
 
-    double area() const override { return width * height; }
-    void draw(ostream &file) const override
+    double area() const override
+    {
+        return width * height;
+    }
+    void draw(ostream& file) const override
     {
         Position position = getPosition();
-        
+
         file << position.x << " " << position.y << " moveto\n";
-        file << position.x + width << " " << position.y << " lineto\n"; 
-        file << position.x + width << " " << position.y + height << " lineto\n"; 
-        file << position.x << " " << position.y + height << " lineto\n"; 
-        file << "closepath\n"; 
+        file << position.x + width << " " << position.y << " lineto\n";
+        file << position.x + width << " " << position.y + height << " lineto\n";
+        file << position.x << " " << position.y + height << " lineto\n";
+        file << "closepath\n";
         file << "fill\n";
     }
 };
@@ -121,16 +147,24 @@ class Drawing
 private:
     ofstream file;
     vector<Shape*> shapes;
+
 public:
-    Drawing(string fileName): file(fileName), shapes() {
-        if (!file.is_open()){ 
+    Drawing(string fileName)
+        : file(fileName)
+        , shapes()
+    {
+        if (!file.is_open())
+        {
             cerr << "File failed to open, check path.\n";
             exit(1);
         }
     }
     ~Drawing()
     {
-        for (auto* s : shapes) { delete s;}
+        for (auto* s : shapes)
+        {
+            delete s;
+        }
     }
 
     void add(Shape* shape)
@@ -157,8 +191,8 @@ public:
     }
 };
 
-
-int main() {
+int main()
+{
     cout << "########" << endl;
     cout << "Main Problem" << endl;
     cout << "########" << endl;
@@ -166,13 +200,13 @@ int main() {
     // set path to create .ps file to write postscript instructions
     Drawing d("/test.ps");
     // set color
-    d.setRGB(1,0,0); // set drawing color to be bright red:  1 0 0 setrgbcolor
+    d.setRGB(1, 0, 0); // set drawing color to be bright red:  1 0 0 setrgbcolor
     // add FilledRect
     d.add(new FilledRect(100.0, 550.0, 200.0, 50)); // x y moveto x y lineto ... fill
     // add Rect
-    d.add(new Rect(200.0, 650.0, 200.0, 50));       // x y moveto x y lineto ... stroke
+    d.add(new Rect(200.0, 650.0, 200.0, 50)); // x y moveto x y lineto ... stroke
     // add Circle
-    d.add(new Circle(300,300, 100)); // centered in 300 , 300 pixel with radius
+    d.add(new Circle(300, 300, 100)); // centered in 300 , 300 pixel with radius
     // draw all shapes added to d
     d.draw();
     // print out all shapes area
