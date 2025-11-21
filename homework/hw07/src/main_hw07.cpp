c0909f88f0f8da9219dd22d2848761fbd1fb75ee
crossfirev <lepismatthew@gmail.com>
2025-11-21 16:30:26 -0500

    Fixing up Shape implementation

--- homework/hw07/src/main_hw07.cpp
+++ homework/hw07/src/main_hw07.cpp
@@ -16,90 +16,21 @@ struct Position
 class Shape
 {
 private:
-    double pos_x, pos_y;
+    Position position; // Units: pixels
 
 public:
-    Shape(double in_pos_x, double in_pos_y): pos_x(in_pos_x), pos_y(in_pos_y) {} // ctor using initializer list
+    Shape(double in_pos_x, double in_pos_y): position{in_pos_x, in_pos_y} {} // ctor using initializer list
     virtual ~Shape() = default;
 
     virtual double area() const = 0;        // Pure virtual since this is set equal to zero.
     virtual void draw(ostream& file) const = 0;   // Pure virtual since this is set equal to zero.
 
-    pair<double, double> getPosition() const
+    Position getPosition() const
     {
-        pair<double, double> position {pos_x, pos_y};
         return position;
     }
+    virtual string getShapeType() const = 0;
 };
-
-
-// -------------- Design your Circle class here ---------------
-// -----------------------------------------------------------
-// Class Circle inherit Class Shape
-// private variable x, y, radius
-// Make the following functions:
-// Circle constructor that take x, y, radius, Note that x and y are assigned to Shape(x,y)
-//  for example constructor_name(double x, double y) : Shape(x,y)
-// draw that take ostream file to write postscript instruction the function return void
-//   postscript:
-//    x y radius  0  360  arc \n
-//    stroke \n
-// for example for x = 200 pixels, y = 3 pixels, and radius 100 pixels. then your function should write this in test.ps
-// stroke
-// 200 300 100 0 360 arc
-// area take no parameters and return double
-// ~Circle deconstruction
-
-
-// -------------- Design your Rect class here ---------------
-// -----------------------------------------------------------
-// Class Rect inherit Class Shape
-// private variable x, y, width, and height
-// Make the following functions:
-// Rect constructor that take x, y,  width,  height
-// draw that take ostream file to write postscript instruction the function return void
-// draw that take ostream file to write postscript instruction the function return void
-//   postscript:
-//    x y moveto \n
-//    x+w y lineto \n
-//    x+w y+h lineto \n
-//    x y+h lineto \n
-//    closepath \n
-//    stroke \n
-// area take no parameters and return double
-// ~Rect deconstruction
-
-
-// -------------- Design your FilledRect class here ---------------
-// -----------------------------------------------------------
-// Class FilledRect inherit Class Shape
-// private variable x, y, width, and height
-// Make the following functions:
-// FilledRect constructor that take x, y,  width,  height
-// draw that take ostream file to write postscript instruction the function return void
-//   postscript:
-//    x y moveto \n
-//    x+w y lineto \n
-//    x+w y+h lineto \n
-//    x y+h lineto \n
-//    closepath \n
-//    fill \n
-// area take no parameters and return double
-// ~FilledRect deconstruction
-
-
-// -------------- Design your Drawing class here ---------------
-// -----------------------------------------------------------
-// private variables ofstream and vector<shape*>
-// Make the following functions:
-// Drawing constructor as following (is given):
-//  Drawing(string filename) : f(filename.c_str()), shapes() {} // where f is ofstream
-// void add that take shape and push_back to vector
-// void draw take no parameters and loop inside vector to rum draw function for each shape class
-// void showArea take no parameters and loop inside vector to run area function for each shape class
-// void setrgb take three double for red, green, and blue and write postscript line to change color
-//   postscript:
-//    r_value g_value b_value setrgbcolor \n
 
 
 int main() {
